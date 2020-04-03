2bbd0a417bf3db2d33f609840574fe83c6fb380a
NilsOveTen <nils.ove.tendenes@acando.no>
2020-04-03 15:19:35 +0200

    feat: add rdf test

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+"""Sphinx configuration."""
+project = "fdk-rdf-parser"
+author = "Nils Ove Tendenes"
+copyright = f"2020, {author}"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx.ext.doctest",
+]
