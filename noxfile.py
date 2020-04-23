272ae4e5d2cb205cf9022f6060632f5aeb4a1588
NilsOveTen <nils.ove.tendenes@acando.no>
2020-04-23 13:00:56 +0200

    feat: nox safety

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,8 @@
+import tempfile
+
 import nox
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 
 @nox.session(python=["3.8", "3.7"])
@@ -31,3 +33,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
