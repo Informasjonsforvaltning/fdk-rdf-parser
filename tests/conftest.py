9ba6ef80439fe8ba45e239bbe789eb5263497016
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-13 16:27:01 +0200

    chore: specific organizations tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,27 @@
 from unittest.mock import Mock
+from urllib import error
 
 import pytest
 from pytest_mock import MockFixture
 
-from .testdata import org_response
+from .testdata import org_response_0
 
 
 @pytest.fixture
 def mock_organizations_client(mocker: MockFixture) -> Mock:
     mock = mocker.patch("urllib.request.urlopen")
-    mock.return_value.__enter__.return_value.read.return_value = org_response
+    mock.return_value.__enter__.return_value.read.return_value = org_response_0
+    return mock
+
+
+@pytest.fixture
+def mock_organizations_error(mocker: MockFixture) -> Mock:
+    mock = mocker.patch("urllib.request.urlopen")
+    mock.side_effect = error.HTTPError(
+        "https://organizations.fellestestkatalog.no/organizations/123456789",
+        404,
+        "Not Found",
+        {},
+        None,
+    )
     return mock
---
+++ tests/test_organization.py
@@ -0,0 +1,88 @@
+from unittest.mock import Mock
+
+from rdflib import Graph
+
+from fdk_rdf_parser.classes import Publisher, PublisherId
+from fdk_rdf_parser.organizations import publisherFromFDKOrgCatalog
+from .testdata import org_response_0, org_response_1
+
+
+def test_publisher_from_get_all() -> None:
+
+    expected = Publisher(
+        uri="https://organizations.fellestestkatalog.no/organizations/123456789",
+        id="123456789",
+        name="Digitaliseringsdirektoratet",
+        orgPath="/STAT/987654321/123456789",
+        prefLabel={
+            "nn": "Digitaliseringsdirektoratet",
+            "nb": "Digitaliseringsdirektoratet",
+            "en": "Norwegian Digitalisation Agency",
+        },
+        organisasjonsform="ORGL",
+    )
+
+    orgsGraph = Graph().parse(data=org_response_0, format="turtle")
+
+    assert (
+        publisherFromFDKOrgCatalog(
+            PublisherId(uri="https://externalurl.org/123456789", id="123456789"),
+            orgsGraph,
+        )
+        == expected
+    )
+
+
+def test_publisher_not_present_in_get_all(mock_organizations_client: Mock) -> None:
+
+    expected = Publisher(
+        uri="https://organizations.fellestestkatalog.no/organizations/123456789",
+        id="123456789",
+        name="Digitaliseringsdirektoratet",
+        orgPath="/STAT/987654321/123456789",
+        prefLabel={
+            "nn": "Digitaliseringsdirektoratet",
+            "nb": "Digitaliseringsdirektoratet",
+            "en": "Norwegian Digitalisation Agency",
+        },
+        organisasjonsform="ORGL",
+    )
+
+    orgsGraph = Graph().parse(data=org_response_1, format="turtle")
+
+    assert (
+        publisherFromFDKOrgCatalog(
+            PublisherId(uri="https://externalurl.org/123456789", id="123456789"),
+            orgsGraph,
+        )
+        == expected
+    )
+
+
+def test_publisher_not_Found(mock_organizations_error: Mock) -> None:
+
+    expected = Publisher(uri="https://externalurl.org/123456789", id="123456789")
+
+    orgsGraph = Graph().parse(data=org_response_1, format="turtle")
+
+    assert (
+        publisherFromFDKOrgCatalog(
+            PublisherId(uri="https://externalurl.org/123456789", id="123456789"),
+            orgsGraph,
+        )
+        == expected
+    )
+
+
+def test_publisher_no_id() -> None:
+
+    expected = Publisher(uri="https://externalurl.org/123456789")
+
+    orgsGraph = Graph().parse(data=org_response_0, format="turtle")
+
+    assert (
+        publisherFromFDKOrgCatalog(
+            PublisherId(uri="https://externalurl.org/123456789"), orgsGraph
+        )
+        == expected
+    )
