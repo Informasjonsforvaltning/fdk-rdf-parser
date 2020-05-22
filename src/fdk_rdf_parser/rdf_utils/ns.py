9c2bd24cfc73cbc4ef38836afc98d45d856657a2
NilsOveTen <nils.ove.tendenes@acando.no>
2020-05-22 06:49:33 +0200

    chore: ns with f-strings

--- src/fdk_rdf_parser/rdf_utils/ns.py
+++ src/fdk_rdf_parser/rdf_utils/ns.py
@@ -2,55 +2,54 @@ from rdflib import URIRef
 
 
 def dcatURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/ns/dcat#" + subString)
+    return URIRef(f"http://www.w3.org/ns/dcat#{subString}")
 
 
 def admsURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/ns/adms#" + subString)
+    return URIRef(f"http://www.w3.org/ns/adms#{subString}")
 
 
 def dcatApNoURI(subString: str) -> URIRef:
-    return URIRef("http://difi.no/dcatno#" + subString)
+    return URIRef(f"http://difi.no/dcatno#{subString}")
 
 
 def owlTimeURI(subString: str) -> URIRef:
-    return URIRef("https://www.w3.org/TR/owl-time/" + subString)
+    return URIRef(f"https://www.w3.org/TR/owl-time/{subString}")
 
 
 def schemaURI(subString: str) -> URIRef:
-    return URIRef("http://schema.org/" + subString)
+    return URIRef(f"http://schema.org/{subString}")
 
 
 def vcardURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/2006/vcard/ns#" + subString)
+    return URIRef(f"http://www.w3.org/2006/vcard/ns#{subString}")
 
 
 def dctURI(subString: str) -> URIRef:
-    return URIRef("http://purl.org/dc/terms/" + subString)
+    return URIRef(f"http://purl.org/dc/terms/{subString}")
 
 
 def dqvURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/ns/dqvNS#" + subString)
+    return URIRef(f"http://www.w3.org/ns/dqvNS#{subString}")
 
 
 def dqvIsoURI(subString: str) -> URIRef:
-    return URIRef("http://iso.org/25012/2008/dataquality/" + subString)
+    return URIRef(f"http://iso.org/25012/2008/dataquality/{subString}")
 
 
 def provURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/ns/prov#" + subString)
+    return URIRef(f"http://www.w3.org/ns/prov#{subString}")
 
 
 def dcatApiURI(subString: str) -> URIRef:
-    return URIRef("http://dcat.no/dcatapi/" + subString)
+    return URIRef(f"http://dcat.no/dcatapi/{subString}")
 
 
 def rovURI(subString: str) -> URIRef:
-    return URIRef("http://www.w3.org/ns/regorg#" + subString)
+    return URIRef(f"http://www.w3.org/ns/regorg#{subString}")
 
 
 def brURI(subString: str) -> URIRef:
     return URIRef(
-        "https://github.com/Informasjonsforvaltning/organization-catalogue/blob/develop/src/main/resources/ontology/organization-catalogue.owl#"  # noqa: B950
-        + subString
+        f"https://github.com/Informasjonsforvaltning/organization-catalogue/blob/develop/src/main/resources/ontology/organization-catalogue.owl#{subString}"  # noqa: B950
     )
