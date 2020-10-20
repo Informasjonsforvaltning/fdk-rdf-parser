fa054a7a40edcb60a80cbf0de32b6389f5e484a6
NilsOveTen <github.nils.ove@tendenes.com>
2020-10-20 08:27:22 +0200

    feat: use nox-poetry to install dependencies in nox sessions

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # fdk-rdf-parser
+
+This is a pypi-package, [fdk-rdf-parser](https://pypi.org/project/fdk-rdf-parser), that parses rdf-data to json-objects used by [Felles dataktatalog](https://data.norge.no)
+
+## Develop and run locally
+### Requirements
+- [pyenv](https://github.com/pyenv/pyenv) (recommended)
+- [poetry](https://python-poetry.org/)
+- [nox](https://nox.thea.codes/en/stable/)
+- [nox-poetry](https://pypi.org/project/nox-poetry/)
+
+### Install software:
+```
+% pyenv install 3.8.6
+% pyenv local 3.8.6
+% pip install poetry==1.1.3
+% pip install nox==2020.8.22
+% pip install nox-poetry==0.5.0
+% poetry install
+```
+
+### Environment variables
+There are two environment variables, ORGANIZATION_CATALOGUE_BASE_URI and REFERENCE_DATA_BASE_URI. The examples here are also default values, nothing is therefore required to run the code.
+
+```
+ORGANIZATION_CATALOGUE_BASE_URI=https://organization-catalogue.staging.fellesdatakatalog.digdir.no
+REFERENCE_DATA_BASE_URI=http://staging.fellesdatakatalog.digdir.no/reference-data
+```
+
+## Running tests
+[pytest](https://docs.pytest.org/en/latest/) is used to run tests.
+
+To run linters, safety, checkers and tests:
+```
+% nox
+```
+
+Code formatting:
+```
+% nox black
+```
+
+### Other helpful commands
+
+Run tests outside of a nox session:
+```
+% poetry run pytest
+```
+
+Run specific nox sessions:
+```
+% nox -s mypy
+% nox -rs lint
+```
+
+Run session with specified arguments:
+```
+% nox -s tests -- -vv
+```
--- noxfile.py
+++ noxfile.py
@@ -1,43 +1,28 @@
 import tempfile
-from typing import Any
 
 import nox
 from nox.sessions import Session
+import nox_poetry
 
 nox.options.sessions = "lint", "mypy", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
-def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install(f"--constraint={requirements.name}", *args, **kwargs)
-
-
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python="3.8")
 def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", "--no-dev", external=True)
     env = {
         "ORGANIZATION_CATALOGUE_BASE_URI": "https://organizations.fellestestkatalog.no"
     }
-    install_with_constraints(
-        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
-    )
-    session.run("pytest", *args, env=env)
+    nox_poetry.install(session, nox_poetry.WHEEL)
+    nox_poetry.install(session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock")
+    session.run("pytest", env=env, *args)
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python="3.8")
 def lint(session: Session) -> None:
     args = session.posargs or locations
-    install_with_constraints(
+    nox_poetry.install(
         session,
         "flake8",
         "flake8-annotations",
@@ -53,7 +38,7 @@ def lint(session: Session) -> None:
 @nox.session(python="3.8")
 def black(session: Session) -> None:
     args = session.posargs or locations
-    install_with_constraints(session, "black")
+    nox_poetry.install(session, "black")
     session.run("black", *args)
 
 
@@ -69,20 +54,20 @@ def safety(session: Session) -> None:
             f"--output={requirements.name}",
             external=True,
         )
-        install_with_constraints(session, "safety")
+        nox_poetry.install(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python="3.8")
 def coverage(session: Session) -> None:
     """Upload coverage data."""
-    install_with_constraints(session, "coverage[toml]", "codecov")
+    nox_poetry.install(session, "coverage[toml]", "codecov")
     session.run("coverage", "xml", "--fail-under=0")
     session.run("codecov", *session.posargs)
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python="3.8")
 def mypy(session: Session) -> None:
     args = session.posargs or locations
-    install_with_constraints(session, "mypy")
+    nox_poetry.install(session, "mypy")
     session.run("mypy", *args)
