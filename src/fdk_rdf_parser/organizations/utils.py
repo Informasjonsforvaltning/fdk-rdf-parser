d1ffaf2376dda185117506749c6c10646a0c7fac
NilsOveTen <github.nils.ove@tendenes.com>
2020-11-02 08:44:28 +0100

    chore: match fdk org uri as publisher

--- src/fdk_rdf_parser/organizations/utils.py
+++ src/fdk_rdf_parser/organizations/utils.py
@@ -21,7 +21,8 @@ def org_path_url(org: str) -> str:
 
 
 def organization_number_from_uri(uri: str) -> Optional[str]:
-    match = re.compile(
-        "https://data.brreg.no/enhetsregisteret/api/enheter/(\\d{9})$"
-    ).match(uri)
+    match_uri_0 = "https://data.brreg.no/enhetsregisteret/api/enheter/"
+    match_uri_1 = base_org_url + "/organizations/"
+    match_str = "^(?:" + match_uri_0 + "|" + match_uri_1 + ")(\\d{9})$"
+    match = re.compile(match_str).match(uri)
     return match.group(1) if match else None
