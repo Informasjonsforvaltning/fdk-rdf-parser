69cc34c0c4f522540af641cc212838a7c9bab35e
Øyvind Isaksen <oyvindisaks1@gmail.com>
2021-02-12 12:36:39 +0100

    fix(event): remove unused extract_events function

--- src/fdk_rdf_parser/parse_functions/event.py
+++ src/fdk_rdf_parser/parse_functions/event.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from rdflib import Graph, URIRef
 from rdflib.namespace import DCTERMS
@@ -8,7 +8,6 @@ from fdk_rdf_parser.rdf_utils import (
     cv_uri,
     is_type,
     object_value,
-    resource_list,
     value_translations,
 )
 from .publisher import extract_authorities_as_publishers
@@ -52,15 +51,3 @@ def parse_event(graph: Graph, subject: URIRef) -> Optional[Event]:
         )
 
     return None
-
-
-def extract_events(
-    graph: Graph, subject: URIRef, predicate: URIRef
-) -> Optional[List[Event]]:
-    values: List[Event] = []
-    for resource in resource_list(graph, subject, predicate):
-        event = parse_event(graph, resource)
-        if event:
-            values.append(event)
-
-    return values if len(values) > 0 else None
