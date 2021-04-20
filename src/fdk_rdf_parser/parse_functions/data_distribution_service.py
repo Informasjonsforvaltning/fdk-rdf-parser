98bcfca58d51b920611f6a4f711ac363f0d10fc7
NilsOveTen <github.nils.ove@tendenes.com>
2021-04-20 12:25:01 +0200

    chore: handle dcat-ap-no 2.0 version of dcatapi

--- src/fdk_rdf_parser/parse_functions/data_distribution_service.py
+++ src/fdk_rdf_parser/parse_functions/data_distribution_service.py
@@ -6,6 +6,7 @@ from rdflib.namespace import DCTERMS
 from fdk_rdf_parser.classes import DataDistributionService
 from fdk_rdf_parser.rdf_utils import (
     dcat_api_uri,
+    dcat_uri,
     resource_list,
     value_translations,
 )
@@ -16,18 +17,30 @@ def extract_data_distribution_services(
     graph: Graph, subject: URIRef
 ) -> Optional[List[DataDistributionService]]:
     values = []
-    for resource in resource_list(graph, subject, dcat_api_uri("accessService")):
+
+    access_services = resource_list(graph, subject, dcat_api_uri("accessService"))
+    access_services.extend(resource_list(graph, subject, dcat_uri("accessService")))
+
+    for resource in access_services:
         resource_uri = None
         if isinstance(resource, URIRef):
             resource_uri = resource.toPython()
+
+        endpoint_description = extract_skos_concept(
+            graph, resource, dcat_uri("endpointDescription")
+        )
 
         values.append(
             DataDistributionService(
                 uri=resource_uri,
                 title=value_translations(graph, resource, DCTERMS.title),
                 description=value_translations(graph, resource, DCTERMS.description),
-                endpointDescription=extract_skos_concept(
-                    graph, resource, dcat_api_uri("endpointDescription")
+                endpointDescription=(
+                    endpoint_description
+                    if endpoint_description
+                    else extract_skos_concept(
+                        graph, resource, dcat_api_uri("endpointDescription")
+                    )
                 ),
             )
         )
