366012f5f510df75c23d2475dde92970c31a9078
Jeff Reiffers <jeffreiffers@gmail.com>
2021-09-10 14:41:26 +0200

    chore: added test for remove_scheme_and_trailing_slash

--- tests/test_reference_data.py
+++ tests/test_reference_data.py
@@ -6,6 +6,7 @@ from fdk_rdf_parser.reference_data import (
     get_data_service_reference_data,
     get_dataset_reference_data,
 )
+from fdk_rdf_parser.reference_data.utils import remove_scheme_and_trailing_slash
 from .testdata import data_service_reference_data, dataset_reference_data
 
 
@@ -47,3 +48,13 @@ def test_get_reference_data_parse_error(
 
     assert get_dataset_reference_data() == expected_dataset
     assert get_data_service_reference_data() == expected_data_service
+
+
+def test_remove_scheme_and_trailing_slash() -> None:
+
+    assert remove_scheme_and_trailing_slash("http://my.uri/") == "my.uri"
+    assert remove_scheme_and_trailing_slash("https://my.uri") == "my.uri"
+    assert remove_scheme_and_trailing_slash("ftp://my.uri/") == "ftp://my.uri"
+    assert remove_scheme_and_trailing_slash("my.uri/") == "my.uri"
+    assert remove_scheme_and_trailing_slash(None) == ""
+    assert remove_scheme_and_trailing_slash("") == ""
