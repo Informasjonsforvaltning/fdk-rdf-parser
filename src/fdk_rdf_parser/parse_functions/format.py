3787927a937c9dbf41f936378e5c8bc7c1eb9d15
NilsOveTen <github.nils.ove@tendenes.com>
2022-02-15 09:16:27 +0100

    fix: add seeAlso for hasFormat in infomodels

--- src/fdk_rdf_parser/parse_functions/format.py
+++ src/fdk_rdf_parser/parse_functions/format.py
@@ -1,13 +1,14 @@
 from typing import List, Optional
 
 from rdflib import Graph, URIRef
-from rdflib.namespace import DCTERMS
+from rdflib.namespace import DCTERMS, RDFS
 
 from fdk_rdf_parser.classes import Format
 from fdk_rdf_parser.rdf_utils import (
     dct_uri,
     object_value,
     resource_list,
+    value_list,
     value_translations,
 )
 from .dcat_resource import extract_skos_code
@@ -23,6 +24,7 @@ def extract_formats(graph: Graph, subject: URIRef) -> Optional[List[Format]]:
                 title=value_translations(graph, resource, DCTERMS.title),
                 format=object_value(graph, resource, dct_uri("format")),
                 language=extract_skos_code(graph, resource, DCTERMS.language),
+                seeAlso=value_list(graph, resource, RDFS.seeAlso),
             )
         )
 
