a7fb1c033a1449e7c2c13e91bc71f7f8801e421c
terjesyl <108797958+terjesyl@users.noreply.github.com>
2023-01-03 12:33:59 +0100

    feat: add organization class

---
+++ src/fdk_rdf_parser/parse_functions/organization.py
@@ -0,0 +1,74 @@
+from typing import (
+    List,
+    Optional,
+)
+
+from rdflib import (
+    Graph,
+    URIRef,
+)
+from rdflib.namespace import (
+    DCTERMS,
+    FOAF,
+    SKOS,
+)
+
+from fdk_rdf_parser.classes import Organization
+from fdk_rdf_parser.rdf_utils import (
+    object_value,
+    org_uri,
+    rov_uri,
+)
+from fdk_rdf_parser.rdf_utils.ns import br_uri
+from fdk_rdf_parser.rdf_utils.utils import (
+    is_type,
+    value_list,
+    value_translations,
+)
+
+
+def parse_organization(graph: Graph, organization_uri: URIRef) -> Organization:
+    organization = Organization(
+        uri=organization_uri.toPython()
+        if isinstance(organization_uri, URIRef)
+        else None,
+        identifier=object_value(graph, organization_uri, DCTERMS.identifier),
+    )
+    if is_type(org_uri("Organization"), graph, organization_uri):
+        organization.name = value_translations(graph, organization_uri, DCTERMS.title)
+        organization.title = value_translations(graph, organization_uri, SKOS.prefLabel)
+        organization.homepage = value_list(graph, organization_uri, FOAF.homepage)
+        organization.spatial = value_list(graph, organization_uri, DCTERMS.spatial)
+        organization.orgType = object_value(graph, organization_uri, DCTERMS.type)
+    elif is_type(rov_uri("RegisteredOrganization"), graph, organization_uri):
+        organization.name = value_translations(
+            graph, organization_uri, rov_uri("legalName")
+        )
+        organization.orgPath = object_value(graph, organization_uri, br_uri("orgPath"))
+        organization.title = value_translations(graph, organization_uri, FOAF.name)
+        organization.homepage = value_list(graph, organization_uri, FOAF.homepage)
+        org_type = object_value(graph, organization_uri, rov_uri("orgType"))
+        organization.orgType = org_type.split("#")[-1] if org_type else None
+
+    return set_organization_name_from_pref_label_if_missing(organization)
+
+
+def extract_organizations(
+    graph: Graph, subject: URIRef, predicate: URIRef
+) -> Optional[List[Organization]]:
+    organization_list = list(
+        map(
+            lambda organization_uri: parse_organization(graph, organization_uri),
+            graph.objects(subject, predicate),
+        )
+    )
+    return organization_list if len(organization_list) > 0 else None
+
+
+def set_organization_name_from_pref_label_if_missing(
+    organization: Organization,
+) -> Organization:
+    if (organization.name and len(organization.name) > 0) or organization.title is None:
+        return organization
+    organization.name = organization.title
+    return organization
---
+++ tests/test_organization.py
@@ -0,0 +1,196 @@
+from rdflib import (
+    Graph,
+    URIRef,
+)
+
+from fdk_rdf_parser.classes import Organization
+from fdk_rdf_parser.parse_functions.organization import (
+    extract_organizations,
+    parse_organization,
+    set_organization_name_from_pref_label_if_missing,
+)
+from fdk_rdf_parser.rdf_utils.ns import cv_uri
+
+
+def test_parse_organization() -> None:
+    src = """
+        @prefix dct:   <http://purl.org/dc/terms/> .
+        @prefix foaf:  <http://xmlns.com/foaf/0.1/> .
+        @prefix skos:  <http://www.w3.org/2004/02/skos/core#> .
+        @prefix org:   <http://www.w3.org/ns/org#> .
+
+        <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123>
+            a                      org:Organization ;
+            dct:identifier         "123" ;
+            dct:title              "Digitaliseringsdirektoratet"@nb ;
+            skos:prefLabel         "Digitaliseringsdirektoratet"@nn , "Digitaliseringsdirektoratet"@nb , "Norwegian Digitalisation Agency"@en ;
+            dct:type               <http://purl.org/adms/publishertype/NationalAuthority> ;
+            foaf:homepage          "https://www.digdir.no" ;
+            dct:spatial            <http://publications.europa.eu/resource/authority/country/NOR> .
+    """
+
+    expected = Organization(
+        uri="https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123",
+        identifier="123",
+        name={"nb": "Digitaliseringsdirektoratet"},
+        title={
+            "nn": "Digitaliseringsdirektoratet",
+            "nb": "Digitaliseringsdirektoratet",
+            "en": "Norwegian Digitalisation Agency",
+        },
+        orgType="http://purl.org/adms/publishertype/NationalAuthority",
+        homepage=["https://www.digdir.no"],
+        spatial=["http://publications.europa.eu/resource/authority/country/NOR"],
+    )
+    graph = Graph().parse(data=src, format="turtle")
+    subject = URIRef(
+        "https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123"
+    )
+
+    assert parse_organization(graph, subject) == expected
+
+
+def test_extract_organizations() -> None:
+    src = """
+        @prefix br:      <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/organization-catalog.owl#> .
+        @prefix cv:      <http://data.europa.eu/m8g/> .
+        @prefix cpsv:    <http://purl.org/vocab/cpsv#> .
+        @prefix dct:     <http://purl.org/dc/terms/> .
+        @prefix foaf:    <http://xmlns.com/foaf/0.1/> .
+        @prefix org:     <http://www.w3.org/ns/org#> .
+        @prefix orgtype: <https://raw.githubusercontent.com/Informasjonsforvaltning/organization-catalog/main/src/main/resources/ontology/org-type.ttl#> .
+        @prefix rov:     <http://www.w3.org/ns/regorg#> .
+        @prefix skos:    <http://www.w3.org/2004/02/skos/core#> .
+
+        <http://public-service-publisher.fellesdatakatalog.digdir.no/services/1>
+            a cpsv:Service ;
+            cv:ownedBy  <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123> ,
+                        <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123456789> ;
+            dct:identifier  "1" .
+
+        <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123>
+            a                      org:Organization ;
+            dct:identifier         "123" ;
+            dct:title              "Sivil organisasjon"@nb ;
+            skos:prefLabel         "SO"@nn , "SO"@nb , "SO"@en ;
+            dct:type               <http://purl.org/adms/publishertype/NonGovernmentalOrganisation> ;
+            foaf:homepage          "https://www.sivilorg.no" ;
+            dct:spatial            <http://publications.europa.eu/resource/authority/country/NOR> .
+
+        <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123456789>
+            a                      rov:RegisteredOrganization ;
+            dct:identifier         "123456789" ;
+            rov:legalName          "Stor registrert organisasjon"@nb ;
+            foaf:name              "SRO"@nn , "SRO"@nb , "SRO"@en ;
+            rov:orgType            orgtype:FLI ;
+            br:orgPath             "/PRIVAT/123456789" ;
+            foaf:homepage          <https://www.storregorg.no> . """
+
+    expected = [
+        Organization(
+            uri="https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123",
+            identifier="123",
+            name={"nb": "Sivil organisasjon"},
+            title={
+                "nn": "SO",
+                "nb": "SO",
+                "en": "SO",
+            },
+            orgType="http://purl.org/adms/publishertype/NonGovernmentalOrganisation",
+            homepage=["https://www.sivilorg.no"],
+            spatial=["http://publications.europa.eu/resource/authority/country/NOR"],
+        ),
+        Organization(
+            uri="https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123456789",
+            identifier="123456789",
+            orgPath="/PRIVAT/123456789",
+            name={"nb": "Stor registrert organisasjon"},
+            title={
+                "nn": "SRO",
+                "nb": "SRO",
+                "en": "SRO",
+            },
+            orgType="FLI",
+            homepage=["https://www.storregorg.no"],
+        ),
+    ]
+
+    graph = Graph().parse(data=src, format="turtle")
+    subject = URIRef(
+        "http://public-service-publisher.fellesdatakatalog.digdir.no/services/1"
+    )
+    assert extract_organizations(graph, subject, cv_uri("ownedBy")) == expected
+
+
+def test_bnode_organization() -> None:
+
+    src = """
+        @prefix cv:      <http://data.europa.eu/m8g/> .
+        @prefix cpsv:    <http://purl.org/vocab/cpsv#> .
+        @prefix dct:     <http://purl.org/dc/terms/> .
+        @prefix org:     <http://www.w3.org/ns/org#> .
+
+        <http://public-service-publisher.fellesdatakatalog.digdir.no/services/1>
+            a                      cpsv:Service ;
+            cv:ownedBy             [
+                a   org:Organization ;
+                dct:identifier         "123" ] . """
+
+    expected = [
+        Organization(identifier="123"),
+    ]
+
+    graph = Graph().parse(data=src, format="turtle")
+    subject = URIRef(
+        "http://public-service-publisher.fellesdatakatalog.digdir.no/services/1"
+    )
+
+    assert extract_organizations(graph, subject, cv_uri("ownedBy")) == expected
+
+
+def test_parse_invalid_organization_type() -> None:
+    src = """
+        @prefix dct:     <http://purl.org/dc/terms/> .
+        @prefix custom:  <http://staging.fellesdatakatalog.digdir.no/custom-prefix> .
+
+        <https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123>
+            a                custom:InvalidType ;
+            dct:identifier   "123" .
+    """
+
+    expected = Organization(
+        uri="https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123",
+        identifier="123",
+    )
+    graph = Graph().parse(data=src, format="turtle")
+    subject = URIRef(
+        "https://organization-catalog.fellesdatakatalog.digdir.no/organizations/123"
+    )
+
+    assert parse_organization(graph, subject) == expected
+
+
+def test_set_name_from_label() -> None:
+    input_0 = Organization(identifier="123456789")
+    expected_0 = Organization(identifier="123456789")
+    input_1 = Organization(
+        identifier="123456789", name={"nb": "Original"}, title={"nb": "Bokmål"}
+    )
+    expected_1 = Organization(
+        identifier="123456789", name={"nb": "Original"}, title={"nb": "Bokmål"}
+    )
+    input_2 = Organization(
+        identifier="123456789", title={"nb": "Bokmål", "nn": "Nynorsk"}
+    )
+    expected_2 = Organization(
+        identifier="123456789",
+        name={"nb": "Bokmål", "nn": "Nynorsk"},
+        title={"nb": "Bokmål", "nn": "Nynorsk"},
+    )
+    input_3 = Organization(identifier="123456789", title={})
+    expected_3 = Organization(identifier="123456789", name={}, title={})
+
+    assert set_organization_name_from_pref_label_if_missing(input_0) == expected_0
+    assert set_organization_name_from_pref_label_if_missing(input_1) == expected_1
+    assert set_organization_name_from_pref_label_if_missing(input_2) == expected_2
+    assert set_organization_name_from_pref_label_if_missing(input_3) == expected_3
