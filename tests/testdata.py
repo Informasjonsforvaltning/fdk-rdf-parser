b429baa652051261054f656df1561ac3d02fc8e9
NilsOveTen <github.nils.ove@tendenes.com>
2024-01-26 15:35:05 +0100

    refactor: rm dependency on publisher type ref-data

--- tests/testdata.py
+++ tests/testdata.py
@@ -2,26 +2,6 @@ from fdk_rdf_parser.classes import ReferenceDataCode
 from fdk_rdf_parser.reference_data import PublicServiceReferenceData
 
 public_service_reference_data = PublicServiceReferenceData(
-    organization_types={
-        "purl.org/adms/publishertype/IndustryConsortium": ReferenceDataCode(
-            uri="http://purl.org/adms/publishertype/IndustryConsortium",
-            code="IndustryConsortium",
-            prefLabel={
-                "nn": "Industrikonsortium",
-                "nb": "Industrikonsortium",
-                "en": "Industry consortium",
-            },
-        ),
-        "purl.org/adms/publishertype/NationalAuthority": ReferenceDataCode(
-            uri="http://purl.org/adms/publishertype/NationalAuthority",
-            code="NationalAuthority",
-            prefLabel={
-                "nn": "Nasjonal myndigheit",
-                "nb": "Nasjonal myndighet",
-                "en": "National authority",
-            },
-        ),
-    },
     types={
         "publications.europa.eu/resource/authority/main-activity/defence": ReferenceDataCode(
             uri="http://publications.europa.eu/resource/authority/main-activity/defence",
